f908e3a9a569a686b0af51f94ab8c46f747e110a
Niclas Nacken <nicki.nacken@gmail.com>
2025-04-26 20:48:22 +0200

    added DHT sensor support

--- DataApi/main.py
+++ DataApi/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+import random
+import adafruit_dht
+import board
 
+sensor = adafruit_dht.DHT22(board.D4)
 app = FastAPI()
 
 @app.get("/")
@@ -8,4 +12,28 @@ def read_root():
 
 @app.get("/temp")
 def read_temp():
-    return {"Temperature": "20C"}
+    result: dict = {}
+
+    for i in range(50):
+        try:
+            temp = sensor.temperature
+            result.update({"temperature": f"{temp}"})
+            break
+        except:
+            print("Error while reading temperature, Attempt: " + i)
+    
+    return result
+
+@app.get("/humidity")
+def read_humidity():
+    result: dict = {}
+
+    for i in range(50):
+        try:
+            humidity = sensor.humidity
+            result.update({"humidity": f"{humidity}"})
+            break
+        except:
+            print("Error while reading humidity, Attempt: " + i)
+
+    return result
