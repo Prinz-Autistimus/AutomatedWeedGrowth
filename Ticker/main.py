52f0780b93f7a7cfca20e35300338b924e907629
Niclas Nacken <nicki.nacken@gmail.com>
2025-05-02 12:31:30 +0200

    changed ticker from using cron to python script for more control during dev

---
+++ Ticker/main.py
@@ -0,0 +1,11 @@
+import requests
+import time
+
+while True:
+    try:
+        response = requests.post("http://192.168.178.68:8000/tick")
+        print(f"Status: {response.status_code}, Antwort: {response.text}")
+    except Exception as e:
+        print(f"Fehler beim Senden: {e}")
+
+    time.sleep(1)
